1aa80bf887f0ced3c0d0555d0fd5e5c68e23dbb0
Taky0ndon <mmaggio89@gmail.com>
2025-10-27 15:13:06 -0400

    moved files into src/ subdirectory

--- find_minimum.py
+++
@@ -1,7 +0,0 @@
-def find_minimum(nums):
-    minimum = float("inf")
-    if not nums: return None
-    for num in nums:
-        if num < minimum:
-            minimum = num
-    return minimum
--- get_sum.py
+++
@@ -1,5 +0,0 @@
-def get_sum(nums: list[int|float]) -> int|float:
-    sum = 0
-    for n in nums:
-        sum += n
-    return sum
