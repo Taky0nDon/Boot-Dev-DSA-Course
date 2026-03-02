b56b31e468c6776d72791d64565a91076da7d742
Taky0ndon <mmaggio89@gmail.com>
2026-03-01 23:31:30 -0500

    created classes for player and asteroids

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,7 +1,10 @@
 import pygame
 
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 VERSION = pygame.version.ver
 def main():
@@ -11,14 +14,33 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+
+    asteroid_field = AsteroidField()
+    player = Player(x=SCREEN_WIDTH/2,
+                    y=SCREEN_HEIGHT/2)
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill((0,0,0))
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+        iteration_time = game_clock.tick(60)
+        dt = iteration_time / 1000
 
 
 if __name__ == "__main__":
